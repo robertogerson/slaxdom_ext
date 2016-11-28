cb8d4d89d9aa4383bf6c503d88d2cb9af7e45e1d
LucasGDS <LucasGDS@users.noreply.github.com>
2016-11-28 20:21:45 -0200

    Update circle.lua

--- examples/circle.lua
+++ examples/circle.lua
@@ -1,8 +1,6 @@
 --load SXMLUA
 local SXMLUA = require ("sxmlua")
 
-local n_rect = 0
-local tam = 150
 local colors = {
   stroke = "green",
   fill = "yellow"
@@ -10,14 +8,6 @@ local colors = {
 
 --table with style definitions
 local style = {
-  ["rect"] = {
-    colors,
-    width = tam,
-    height = function ()
-               n_rect = n_rect+1
-               return n_rect * 100
-             end 
-  },
   [".mycircle"] = {
     colors
   }
