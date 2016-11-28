910916a144a5d1aae46af56371a7a7eee79d8a84
LucasGDS <LucasGDS@users.noreply.github.com>
2016-11-28 20:22:35 -0200

    Update circle_style_attr.lua

--- examples/circle_style_attr.lua
+++ examples/circle_style_attr.lua
@@ -1,21 +1,11 @@
 local SXMLUA = require ("sxmlua")
 
-local n_rect = 0
-local tam = 150
 local colors = {
   stroke = "green",
   fill = "yellow"
 }
 
 local style = {
-  ["rect"] = {
-    colors,
-    width = tam,
-    height = function ()
-               n_rect = n_rect+1
-               return n_rect * 100
-             end 
-  },
   [".mycircle"] = {
     colors
   }
